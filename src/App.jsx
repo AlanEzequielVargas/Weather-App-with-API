31c3976b638350c43927491defda8f8450fbb19d
Alan Ezequiel Vargas <alanv3533@gmail.com>
2022-10-31 00:44:56 -0300

    only made for Iphone X

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import './App.css'
+import useWeather from './hooks/useWeather';
+
+function App() {
+   //importando respuesta de la API , funcion de cambio de temperatura del boton y verificacion de temperatura para modificacion 
+   const {weather , changeTemp , isTemp} = useWeather();
+
+   //hourIcon es para modificar instantaneamente los colores y fondo segun el horario
+   const hourIcon = weather.weather?.[0]?.icon
+
+   document.body.classList = `${hourIcon?.includes("n") ? 'night-body-background' : 'day-body-background'}`
+
+   //Para realizar una carga antes de mostrar la respuesta y que no se vea incompleta
+   const [load,setLoad] = useState(false)
+   useEffect(()=>{
+      setLoad(true)
+      setTimeout(()=>{
+         setLoad(false)
+      },5000)
+   },[])
+  
+   return (
+      <>
+      {load ? 
+      <img src="https://media.tenor.com/On7kvXhzml4AAAAj/loading-gif.gif" alt="icon weather" /> : 
+      <div className={`App ${hourIcon?.includes("n") ? 'night' : 'day'}`}>
+         <h2 className='city'>{`${weather.name}, ${weather.sys?.country}`}</h2>
+         <img src={`http://openweathermap.org/img/wn/${hourIcon}@4x.png`} alt="icon weather" />
+         <h2 className='temperature'>{`${Math.floor(weather.main?.temp)}${isTemp ? 'C°' : 'F°'}`}</h2>
+         <h2 className='description'>"{weather.weather?.[0]?.description}"</h2>
+         <h2 className='wind'>Wind Speed: <span>{weather.wind?.speed}</span></h2>
+         <h2 className='humidity'>Humidity: <span>{weather.main?.humidity}%</span></h2>
+         <h2 className='clouds'>Clouds: <span>{weather.clouds?.all}%</span></h2>
+         <h2 className='pressure'>Pres: <span>{weather.main?.pressure}/hPa</span></h2>
+         <button onClick={changeTemp}>C° to F°</button>
+   </div>
+   }
+      </>
+      
+   ) 
+}
+
+export default App
---
+++ src/hooks/useWeather.jsx
@@ -0,0 +1,41 @@
+import { useState,useEffect } from "react";
+import axios from 'axios'
+
+function useWeather(){
+   const [ weather , setWeather] = useState({});
+   const [isTemp , setIsTemp] = useState(true)
+
+   function changeTemp(){
+      setIsTemp(!isTemp)
+   }
+
+   useEffect(()=>{
+      
+      function success(position){
+         const crd = position.coords;
+         const lat = crd.latitude;
+         const lon = crd.longitude;
+         axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7602e6a4f459144e604fdcea42d16f36${isTemp ? '&units=metric' : '&units=imperial'}`)
+         .then(res=>setWeather(res.data))
+      }  
+
+      function error(err) {
+         alert(`ERROR(${err.code}): ${err.message}`);
+      }
+
+      function options(){
+         const options = {
+         enableHighAccuracy: true,
+         timeout: 1000,
+         maximumAge: infinity
+       }
+       return options.enableHighAccuracy
+      } 
+      
+      navigator.geolocation.getCurrentPosition(success, error, options);
+
+   },[isTemp]);
+
+   return{weather , changeTemp , isTemp}
+}
+export default useWeather;
